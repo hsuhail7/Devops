9c1bbd20ea040135c7840bbe1847c6ad3f71201f
hsuhail7 <mohd.haseeb87@outlook.com>
2023-08-11 06:53:03 +0000

    Completed Day5 Task

---
+++ Day5/backup_work.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#This script will backup files from a particular directory as this is just for the Task purpose
+
+#Since this is a Task so I am taking backup of only Day5 Directory and also keeping the backup in the same Directory
+
+tar -cvpzf /home/ubuntu/Learning_Devops/Learning_Devops/Day5/Backup_Work/backup_of_Day5.tar.gz /home/ubuntu/Learning_Devops/Learning_Devops/Day5
+
